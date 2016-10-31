f078c1d7f1ef34d15c63f17eb4fb99d6fedbd227
Howon Lee <kck4156@outlook.com>
2016-10-31 16:16:14 +0900

    Initial instance support. (generate, init) Expermental support for default methods. (in java) Added 'toString' default method.

--- ATLuaClass/class.lua
+++ ATLuaClass/class.lua
@@ -1,3 +1,24 @@
 local _M = {}
 
+-- defaultMethods should be overrided when calling generateInstance.
+local defaultMethods = {}
+
+local init = function()
+    local instance = {}
+end
+
+local generateInstance = function(initInstance)
+    -- Since instance system of lua 5.1 doesn't support iterating
+    -- All methods should be overrided manually.
+    
+    function instance:tostring()
+        return defaultMethods.tostring(self)
+    end
+end
+
+defaultMethods.toString = function(instance)
+    return tostring(instance);
+end
+
+_M.init = init
 return _M
