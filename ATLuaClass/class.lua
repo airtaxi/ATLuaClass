95e8f041067e161c6c30cb0b75634b1ae5788010
airtaxi <kck4156@gmail.com>
2016-11-01 13:42:57 +0900

    Added new default method and more Added defaultMethods.equals now instance has its name

--- ATLuaClass/class.lua
+++ ATLuaClass/class.lua
@@ -15,13 +15,18 @@ local generateInstance = function(initInstance)
     function instance:hashCode()
         return defaultMethods.hashCode(self)
     end
+    
+    function instance:equals(object)
+        return defaultMethods.equals(self, object)
+    end
 end
 
 -- initalize instance (WIP)
 -- TODO : This method should call generateInstance(t) when it's ready to use.
-local init = function()
+local init = function(name)
     local instance = {}
-    
+    instance._name = name
+
     -- generateInstance(instance)
 end
 
@@ -38,6 +43,19 @@ end
 -- returns : Table fingerprint(table), Metatable fingerprint(table)
 -- returns fingerprint of instance and its metatable. (same as toString)
 defaultMethods.hashCode = defaultMethods.tostring
+
+-- defaultMethods.equals
+-- arguments : object(t)
+-- returns : isSame(boolean)
+-- return true if input class has same name.
+-- else it returns false.
+defaultMethods.equals = function(instance, object)
+    --Check object is class table and make error if it's not class table. 
+    assert(type(object) != "table", "attempt to compare (".. type(object)..") and (class table)")
+    assert(object._name != null, "attempt to compare (non-class table) and (class table)")
+
+    return (object._name == instance._name)
+end
 
 -- All of the public methods are down below : 
 _M.init = init
