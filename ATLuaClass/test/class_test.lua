85ed8c15187e6ef12163f0318a8b518f6b5b44f8
Howon Lee <kck4156@outlook.com>
2016-11-14 15:48:26 +0900

    Added test case class test class #1

--- ATLuaClass/test/class_test.lua
+++ ATLuaClass/test/class_test.lua
@@ -6,3 +6,21 @@ local ATLuaClass = require("init")
 
 -- Class test case #1
 -- TODO: implement feature
+
+-- local ATLuaClass = require("ATLuaClass.init")
+local Math = ATLuaClass.init("Math")
+
+Math:addMethod({name="add", type="private", isAbstract=false}, function(this, a, b)
+    return a+b
+end)
+
+Math:addMethod({name="multiply", type="public", isAbstract=false}, function(this, a, b)
+    local returnVar = 0
+    for(i = 1, b, 1) do
+        returnVar = this.add(returnVar,a)
+    end
+    return returnVar
+end)
+
+print(Math:multiply(5,3))
+--> 15
