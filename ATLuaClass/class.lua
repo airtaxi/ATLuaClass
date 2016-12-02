c2e6e0f9184971e3eed2acf9e2dce4a920809526
airtaxi <kck4156@gmail.com>
2016-12-02 16:48:03 +0900

    Added inherit feature

--- ATLuaClass/class.lua
+++ ATLuaClass/class.lua
@@ -12,26 +12,29 @@ local init = function(name, arg)
 	-- TODO : implement inherit feature
 	local class = {}
 	local constructor
-	local inheritFrom
-	if(type(arg) == "table") then
-		inheritFrom = arg
+	if(type(arg) == "string") then
+		local inheritClass = instances[arg]
+		assert(inheritClass ~= nil, "ATLuaClass : inherit class should not be a nil value")
+		for k,v in pairs(inheritClass) do
+			if type(k) == "string" and type(k) ~= "new" then
+				class[k] = v
+			end
+		end
 	elseif(type(arg) == "function") then
 		constructor = arg
 	end
 	instances[name] = class
 	class.new = function(...)
-		local instance = class
 		local retInstance = {}
 		if(constructor ~= nil) then
 			local args = {...}
 			constructor(retInstance, unpack(args))
 		end
-		for k,v in pairs(instance) do
+		for k,v in pairs(class) do
 			if type(k) == "string" and type(k) ~= "new" then
 				retInstance[k] = v
 			end
 		end
-		instance[#instance+1] = retInstance
 		return retInstance
 	end
 	return class
