c719b2c0f83af2cb99cb8452622b1beb51969680
airtaxi <kck4156@gmail.com>
2016-12-05 18:47:20 +0900

    Added test binaries

---
+++ ATLuaClass/test/private_test.lua
@@ -0,0 +1,29 @@
+-- Adding parent path into package.path since test is in subdirectory of module.
+package.path = package.path .. ";../?.lua"
+
+-- since we added parent directory into package.path, we require "class" in this case.
+
+-- test case #1 : Basic access of private member variable.
+local ATLuaClass = require("class")
+print("test case #1 : Basic access of private member variable.")
+local TestClass = ATLuaClass.init("Class")
+
+function TestClass:addPrivate()
+	self.setPrivate("var1", 123)
+end
+function TestClass:getPrivateValue()
+	local retVar = self.getPrivate("var1")
+	return retVar
+end
+
+local testClass = TestClass:new()
+
+testClass:addPrivate()
+
+print(testClass.getPrivate("var1")) -- error
+print(testClass:getPrivateValue()) -- 123
+
+print("print(testClass.getPrivate(\"var1\")) => " ..
+((select(1,pcall(function() print(testClass.getPrivate("var1")) end))) and "" or "Error"))
+
+print("print(testClass:getPrivateValue() => " .. testClass:getPrivateValue())
